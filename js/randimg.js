3083f9ff9c268e89196e05b1613deb3b1dde0dc8
Alec <hodgka@rpi.edu>
2016-08-01 23:46:58 -0800

    random image works on all pages

--- js/randimg.js
+++ js/randimg.js
@@ -1,23 +1,9 @@
-window.onload = choosePic;
-
-var pictures = new Array("img/dover.jpg","img/moon-mountain-background.jpg","img/mountain-background.jpg");
-
-function choosePic() {
-
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("minitron").style.backgroundImage = "url('" + pictures[randomNum] + "')";
-
-
-function surprise(cb) {
-    (function loop() {
-        var now = new Date();
-        if (now.getDate() === 12 && now.getHours() === 12 && now.getMinutes() === 0) {
-            cb();
-        }
-        now = new Date();                  // allow for time passing
-        var delay = 60000 - (now % 60000); // exact ms to next minute interval
-        setTimeout(loop, delay);
-    })();
+function randomImage(){
+    var images = ['img/dover.jpg','img/moon-mountain-background.jpg','img/mountain-background.jpg'];
+    var size = images.length;
+    var x = Math.floor(size*Math.random());
+    console.log(x);
+    var element = document.getElementsByClassName("background");
+    console.log(element);
+    element[0].style["background-image"] = "url(" + images[x]+")";
 }
-
-surprise(choosePic());
